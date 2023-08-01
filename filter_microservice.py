00d4ff808e819cefb1d0cc51254aa4666cb325fa
helen-m-wong <108026042+helen-m-wong@users.noreply.github.com>
2023-07-31 22:27:12 -0500

    Add files via upload

---
+++ filter_microservice.py
@@ -0,0 +1,22 @@
+import json 
+
+# Opens JSON file
+expenses_json = open('expenses.json')
+# Returns JSON object as a dictionary
+expenses_list = json.load(expenses_json)
+# Retrieves expenses data
+expenses_data = expenses_list["data"]
+# Retrieves filter category
+filter = expenses_list["filter"]
+filtered_list = []
+
+# Adds expenses to filtered list
+for expense in expenses_data:
+    if expense["category"] == filter:
+        filtered_list.append(expense)
+
+# Converts filtered list to JSON object
+filtered_json = json.dumps(filtered_list)
+# Opens JSON file and writes filtered JSON object to file
+with open("filtered_expenses.json", "w") as outfile:
+    outfile.write(filtered_json)
